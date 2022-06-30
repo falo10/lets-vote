5d9bc49c09293cc581c72dfb6406e33c8a349d4a
falo10 <swiecinskiolaf@gmail.com>
2022-06-30 20:22:07 +0200

    Initial commit

--- letsVote/letsVote/settings.py
+++ letsVote/letsVote/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-t6i(5nt04d@clbc5j5@u+f7%=)*hsmc)lj1-ddy*@*$c%*0*lf'
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
